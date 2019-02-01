ac6264284a7884f3e51310a036ab5448102fd345
theultimatechallenge <46925408+theultimatechallenge@users.noreply.github.com>
2019-02-01 20:59:21 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 23, 2019 08:37:25").getTime();
+var countDownDate = new Date("Feb 23, 2019 08:00:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "YOU LOSE";
   }
 }, 1000);
