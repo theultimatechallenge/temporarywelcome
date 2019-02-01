51487ff2cfa6a0c694939306fe8cdc7a0a009b62
theultimatechallenge <46925408+theultimatechallenge@users.noreply.github.com>
2019-02-01 20:56:26 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 23, 2019 15:37:25").getTime();
+var countDownDate = new Date("Feb 23, 2019 08:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -17,7 +17,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = "COUNTDOWN: " + days + "d " + hours + "h "
+  document.getElementById("demo").innerHTML = "LAUNCH COUNTDOWN: " + days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
